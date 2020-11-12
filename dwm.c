0c4b5034b2d35b6a5fd883f3ed9dd3a97db5cae4
Jason Hsu <jasonhsu2013@gmail.com>
2020-11-12 01:35:58 -0800

    Include broken tilemovemouse code

--- dwm.c
+++ dwm.c
@@ -176,6 +176,8 @@ static int gettextprop(Window w, Atom atom, char *text, unsigned int size);
 static void grabbuttons(Client *c, int focused);
 static void grabkeys(void);
 static void incnmaster(const Arg *arg);
+static void insertafter(Client *a, Client *b);
+static void insertbefore(Client *a, Client *b);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
 static void manage(Window w, XWindowAttributes *wa);
@@ -210,6 +212,7 @@ static void spawn(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tile(Monitor *);
+static void tilemovemouse(const Arg *arg);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void toggletag(const Arg *arg);
@@ -984,6 +987,24 @@ isuniquegeom(XineramaScreenInfo *unique, size_t n, XineramaScreenInfo *info)
 #endif /* XINERAMA */
 
 void
+insertafter(Client *a, Client *b)	/* insert a after b in the client list */
+{
+	a->next = b->next;
+	b->next = a;
+}
+
+void
+insertbefore(Client *a, Client *b)	/* insert a before b in the client list */
+{
+	Client **x = &clients;
+	
+	while(*x != b && *x)
+		x = & (*x)->next;
+	*x = a;
+	a->next = b;
+}
+
+void
 keypress(XEvent *e)
 {
 	unsigned int i;
@@ -1697,6 +1718,50 @@ tile(Monitor *m)
 			if (ty + HEIGHT(c) < m->wh)
 				ty += HEIGHT(c);
 		}
+}
+
+void
+tilemovemouse(const Arg *arg) {
+	/* Could EnterNotify events be used instead? */
+	Client *c, *d;
+	XEvent ev;
+	int x, y;
+	Bool after;
+
+	if(!(c = sel))
+		return;
+	if(c->isfloating || !lt[sellt]->arrange){
+		movemouse(NULL);
+		return;
+	}
+	if(XGrabPointer(dpy, root, False, MOUSEMASK, GrabModeAsync, GrabModeAsync,
+	None, cursor[CurMove], CurrentTime) != GrabSuccess)
+		return;
+	do {
+		XMaskEvent(dpy, MOUSEMASK|ExposureMask|SubstructureRedirectMask, &ev);
+		switch (ev.type) {
+		case ConfigureRequest:
+		case Expose:
+		case MapRequest:
+			handler[ev.type](&ev);
+			break;
+		case MotionNotify:
+			x = ev.xmotion.x;
+			y = ev.xmotion.y;
+			after = False;
+			for(d = nexttiled(clients); d; d = nexttiled(d->next)){
+				if(d == c)
+					after = True;
+				else if(INRECT(x, y, d->x, d->y, d->w+2*borderpx, d->h+2*borderpx)){
+					detach(c);
+					after ? insertafter(c, d) : insertbefore(c,d);
+					arrange();
+					break;
+				}
+			}
+		}
+	} while(ev.type != ButtonRelease);
+	XUngrabPointer(dpy, CurrentTime);
 }
 
 void
