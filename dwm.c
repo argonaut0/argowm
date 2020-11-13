90eb6f099d500a5daa9d968407752c2b8b06e773
Jason Hsu <jasonhsu2013@gmail.com>
2020-11-12 21:09:25 -0800

    Change statusbar name

--- dwm.c
+++ dwm.c
@@ -2061,7 +2061,7 @@ void
 updatestatus(void)
 {
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-		strcpy(stext, "dwm-"VERSION);
+		strcpy(stext, "argo-dwm "VERSION);
 	drawbar(selmon);
 }
 
